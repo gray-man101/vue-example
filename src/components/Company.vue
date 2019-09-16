6a3334b6b3cfaf1a3d0d5097e9d7e2c8b86262d9
Aleksej Voroncov <a.voroncovs@bill.me>
2019-09-16 18:31:07 +0300

    indeed responsive grid

--- src/components/Company.vue
+++ src/components/Company.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <router-link to="/">Home</router-link>
-    <div class="md-alignment-center">
-      <div class="md-layout-item md-xlarge-size-66 md-large-size-66 md-medium-size-80 md-small-size-90 md-xsmall-size-100">
+    <div class="md-layout md-alignment-center">
+      <div class="md-layout-item md-size-66 md-medium-size-80 md-small-size-90 md-xsmall-size-100">
         <md-card>
           <md-card-area>
             <md-card-media>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,120 +2,124 @@
   <div class="hello">
     <router-link to="/">Home</router-link>
     <div class="md-layout md-gutter md-alignment-center">
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-        <md-card>
-          <md-card-area>
-            <md-card-media>
-              <img src="../assets/logo.png" alt="People">
-            </md-card-media>
+      <div class="md-layout-item md-size-66 md-medium-size-80 md-small-size-90 md-xsmall-size-100">
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item md-size-33 md-small-size-50 md-xsmall-size-100">
+            <md-card>
+              <md-card-area>
+                <md-card-media>
+                  <img src="../assets/logo.png" alt="People">
+                </md-card-media>
 
-            <md-card-header>
-              <div class="md-title">Actions left aligned</div>
-              <div class="md-subhead">Subtitle here</div>
-            </md-card-header>
+                <md-card-header>
+                  <div class="md-title">Actions left aligned</div>
+                  <div class="md-subhead">Subtitle here</div>
+                </md-card-header>
 
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-            </md-card-content>
-          </md-card-area>
+                <md-card-content>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </md-card-content>
+              </md-card-area>
 
-          <md-card-actions md-alignment="left">
-            <md-button to="/company/1">Learn more</md-button>
-            <md-button>Action</md-button>
-          </md-card-actions>
-        </md-card>
-      </div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-        <md-card>
-          <md-card-area>
-            <md-card-media>
-              <img src="../assets/logo.png" alt="People">
-            </md-card-media>
+              <md-card-actions md-alignment="left">
+                <md-button to="/company/1">Learn more</md-button>
+                <md-button>Action</md-button>
+              </md-card-actions>
+            </md-card>
+          </div>
+          <div class="md-layout-item md-size-33 md-small-size-50 md-xsmall-size-100">
+            <md-card>
+              <md-card-area>
+                <md-card-media>
+                  <img src="../assets/logo.png" alt="People">
+                </md-card-media>
 
-            <md-card-header>
-              <div class="md-title">Actions left aligned</div>
-              <div class="md-subhead">Subtitle here</div>
-            </md-card-header>
+                <md-card-header>
+                  <div class="md-title">Actions left aligned</div>
+                  <div class="md-subhead">Subtitle here</div>
+                </md-card-header>
 
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-            </md-card-content>
-          </md-card-area>
+                <md-card-content>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </md-card-content>
+              </md-card-area>
 
-          <md-card-actions md-alignment="left">
-            <md-button to="/company/2">Learn more</md-button>
-            <md-button>Action</md-button>
-          </md-card-actions>
-        </md-card>
-      </div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-        <md-card>
-          <md-card-area>
-            <md-card-media>
-              <img src="../assets/logo.png" alt="People">
-            </md-card-media>
+              <md-card-actions md-alignment="left">
+                <md-button to="/company/2">Learn more</md-button>
+                <md-button>Action</md-button>
+              </md-card-actions>
+            </md-card>
+          </div>
+          <div class="md-layout-item md-size-33 md-small-size-50 md-xsmall-size-100">
+            <md-card>
+              <md-card-area>
+                <md-card-media>
+                  <img src="../assets/logo.png" alt="People">
+                </md-card-media>
 
-            <md-card-header>
-              <div class="md-title">Actions left aligned</div>
-              <div class="md-subhead">Subtitle here</div>
-            </md-card-header>
+                <md-card-header>
+                  <div class="md-title">Actions left aligned</div>
+                  <div class="md-subhead">Subtitle here</div>
+                </md-card-header>
 
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-            </md-card-content>
-          </md-card-area>
+                <md-card-content>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </md-card-content>
+              </md-card-area>
 
-          <md-card-actions md-alignment="left">
-            <md-button to="/company/3">Learn more</md-button>
-            <md-button>Action</md-button>
-          </md-card-actions>
-        </md-card>
-      </div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-        <md-card>
-          <md-card-area>
-            <md-card-media>
-              <img src="../assets/logo.png" alt="People">
-            </md-card-media>
+              <md-card-actions md-alignment="left">
+                <md-button to="/company/3">Learn more</md-button>
+                <md-button>Action</md-button>
+              </md-card-actions>
+            </md-card>
+          </div>
+          <div class="md-layout-item md-size-33 md-small-size-50 md-xsmall-size-100">
+            <md-card>
+              <md-card-area>
+                <md-card-media>
+                  <img src="../assets/logo.png" alt="People">
+                </md-card-media>
 
-            <md-card-header>
-              <div class="md-title">Actions left aligned</div>
-              <div class="md-subhead">Subtitle here</div>
-            </md-card-header>
+                <md-card-header>
+                  <div class="md-title">Actions left aligned</div>
+                  <div class="md-subhead">Subtitle here</div>
+                </md-card-header>
 
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-            </md-card-content>
-          </md-card-area>
+                <md-card-content>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </md-card-content>
+              </md-card-area>
 
-          <md-card-actions md-alignment="left">
-            <md-button to="/company/4">Learn more</md-button>
-            <md-button>Action</md-button>
-          </md-card-actions>
-        </md-card>
-      </div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-        <md-card>
-          <md-card-area>
-            <md-card-media>
-              <img src="../assets/logo.png" alt="People">
-            </md-card-media>
+              <md-card-actions md-alignment="left">
+                <md-button to="/company/4">Learn more</md-button>
+                <md-button>Action</md-button>
+              </md-card-actions>
+            </md-card>
+          </div>
+          <div class="md-layout-item md-size-33 md-small-size-50 md-xsmall-size-100">
+            <md-card>
+              <md-card-area>
+                <md-card-media>
+                  <img src="../assets/logo.png" alt="People">
+                </md-card-media>
 
-            <md-card-header>
-              <div class="md-title">Actions left aligned</div>
-              <div class="md-subhead">Subtitle here</div>
-            </md-card-header>
+                <md-card-header>
+                  <div class="md-title">Actions left aligned</div>
+                  <div class="md-subhead">Subtitle here</div>
+                </md-card-header>
 
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-            </md-card-content>
-          </md-card-area>
+                <md-card-content>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </md-card-content>
+              </md-card-area>
 
-          <md-card-actions md-alignment="left">
-            <md-button to="/company/5">Learn more</md-button>
-            <md-button>Action</md-button>
-          </md-card-actions>
-        </md-card>
+              <md-card-actions md-alignment="left">
+                <md-button to="/company/5">Learn more</md-button>
+                <md-button>Action</md-button>
+              </md-card-actions>
+            </md-card>
+          </div>
+        </div>
       </div>
     </div>
   </div>
